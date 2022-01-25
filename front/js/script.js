da2ed32790b7370ea43cc31c2ae0bfbe3a06db1a
skippynea <stvb@nautile.nc>
2022-01-26 10:51:22 +1100

    Cards creation

--- front/js/script.js
+++ front/js/script.js
@@ -5,8 +5,71 @@
         <p class ="productDescription">Dis enim malesuada risus sapien gravida nulla nisl arcu.Dis enim malesuada risus sapien gravida nulla nisl arcu.</p>
     </article>
 </a> */
-fetch('http://localhost:3000/api/products/')
-    .then((reponse) => reponse.json())
-    .then((data)=>console.log(data))
-    .catch((error) =>console.log(error));
 
+
+// getting the data from server with an URL
+const dataURL = 'http://localhost:3000/api/products/'; 
+
+// fetch is calling the server
+fetch(dataURL)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    makeCards(data);
+    // this function make the cards
+  })
+  .catch((error) => console.log(error));
+
+  // send an error message if something is wrong
+
+
+// Creating a function to make all the cards to displays the products
+
+function makeCards(dataArr) {
+    // stash a reference to container on the page
+    const items = document.getElementById('items');
+    
+    // Boucle for iteration to make a card out of each data object
+    const length = dataArr.length
+    
+    for(let i = 0; i < length; i++) {
+      const card = makeCard(dataArr[i]);
+      items.appendChild(card);
+    }
+  
+  }
+
+// Using the function to create a card for each product
+function makeCard(productObj) {
+// create the elements with all parameters for each product
+const card = document.createElement('a');
+const article = document.createElement('article');
+const img = document.createElement('img');
+const title = document.createElement('h3');
+const description = document.createElement('p');
+
+// add attributes setAttribute('type', button);
+// add classes - classlist.add('')
+
+// for the a href need to use query parameters to link
+// each product to its specific id for ONE product page
+
+const productURL = './product.html?id=' + productObj._id;
+card.setAttribute('href', productURL);
+card.classList.add('items');
+
+img.setAttribute('src', productObj.imageUrl);
+img.setAttribute('alt', productObj.altTxt);
+
+title.innerHTML = productObj.name;
+title.classList.add('productName');
+
+// append tags to article
+article.appendChild(img);
+article.appendChild(title);
+
+// append article to a tag
+card.appendChild(article);
+
+return card;
+}
